a93d616220a7c01c6a84715d470b74980513abe7
Romulo91 <romulo.galiao@googlemail.com>
2022-09-20 21:59:49 +0200

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import Head from 'next/head'
-import Link from "next/link";
-import styles from "../styles/Home.module.scss";
-
-
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
-        <title>Coro Wenzel</title>
-        <meta name='description' content="Homepage from caro wenzel" />
-        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100&display=swap" rel="stylesheet"></link>
+        <title>Coro Wenzel </title>
+        <meta name="description" content="Homepage from caro wenzel" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main className={styles.home}>
-          <div className={styles.header}>
-            <h1>Carolin Wenzel</h1>
-          </div>
+        <div className={styles.header}>
+          <h1>Carolin Wenzel</h1>
+        </div>
 
-          <div className={styles.enter}>
-            <Link href='/about'>
-              <a>Enter</a>
-            </Link>
-          </div>
+        <div className={styles.enter}>
+          <Link href="/about">
+            <a>Enter</a>
+          </Link>
+        </div>
       </main>
-
     </div>
-  )
+  );
 }
